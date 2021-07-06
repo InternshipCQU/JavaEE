0554c9cc8f745a81fd4e13ce452585d87b2f568d
陈江渝 <lasuerte@chenjiangyudeMacBook-Pro.local>
2021-07-06 11:42:05 +0800

    init --cjy

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JavaEE
+环境: IDEA(pro) mysql 
+框架: springMVC框架 前端: jsp（找模版）
+实现
---
+++ onlineCar/src/main/java/com/example/demo/controller/controllerDemo.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.jvm.hotspot.HelloWorld;
+
+@RestController
+public class controllerDemo {
+    @RequestMapping("/Hello")
+    public String HelloWorld(){
+        return "Hello World";
+    }
+}
