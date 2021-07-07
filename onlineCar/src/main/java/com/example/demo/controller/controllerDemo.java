d5337a1c62e4fd9142c9c20086ef8895fb1b6196
Confus-ing <747709318@qq.com>
2021-07-07 17:00:36 +0800

    update --wyc

--- onlineCar/src/main/java/com/example/demo/controller/controllerDemo.java
+++ onlineCar/src/main/java/com/example/demo/controller/controllerDemo.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.HelloWorld;
+
 
 @RestController
 public class controllerDemo {
