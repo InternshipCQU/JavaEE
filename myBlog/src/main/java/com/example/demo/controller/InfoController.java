84c1f57992665a4e755b23a44792a9fca27baabf
cquljs <1022440144@qq.com>
2021-07-09 19:34:35 +0800

    merge. --ljs

--- myBlog/src/main/java/com/example/demo/controller/InfoController.java
+++ myBlog/src/main/java/com/example/demo/controller/InfoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
@@ -18,6 +19,8 @@ public class InfoController {
     private setService setss;
     private getService getss;
 
+    @RequestMapping("/timeline")
+    public String to(){return "timeline";}
     @GetMapping("/infocenter")
     public String setuser(@RequestParam("username") String username, @RequestParam("gender") String gender,
                           @RequestParam("password") String password, @RequestParam("userID") int userID,
