5c52468c032d08a7fd7482d533e2a504c734cdb5
cquljs <1022440144@qq.com>
2021-07-14 11:07:52 +0800

    abc

--- myBlog/src/main/java/com/example/demo/controller/BlogListController.java
+++ myBlog/src/main/java/com/example/demo/controller/BlogListController.java
@@ -2,6 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class BlogListController {
@@ -9,4 +13,7 @@ public class BlogListController {
     public String bloglist(){
         return "bloglist";
     }
+
+    @RequestMapping(value="/deletemyblog",method = RequestMethod.GET)
+    public void deletemyblog(HttpServletRequest request, HttpServletResponse response,){}
 }
